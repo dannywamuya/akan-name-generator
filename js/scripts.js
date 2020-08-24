f82db7964e3678b91b30a48416d352e3864c6ecf
dannywamuya <dwamuya@gmail.com>
2020-08-24 08:54:00 +0300

    Temporary Commit

--- js/scripts.js
+++ js/scripts.js
@@ -4,33 +4,41 @@ let days = ['Sunday' , 'Monday' , 'Tuesday' , 'Wednesday' , 'Thursday' , 'Friday
 
 var year, month, date, cc, yy, day, result;
 
-year = prompt("Birth year: ")
-month = parseInt(prompt("Birth month: "))
-date = parseInt(prompt("Birth date: "))
-cc = parseInt(year.charAt(0) + year.charAt(1))
-yy = parseInt(year.charAt(2) + year.charAt(3))
-result = findDay()
+function getInput(){
+    year = document.getElementById("year").innerHTML;
+    month = parseInt(document.getElementById("month")).innerHTML;
+    date = parseInt(document.getElementById("date")).innerHTML;
+    cc = parseInt(year.charAt(0) + year.charAt(1))
+    yy = parseInt(year.charAt(2) + year.charAt(3))
+    result = findDay()
+
+    if(year == "" && year > 2020 ){
+        alert("Enter valid year")
+        return false;
+    }else if (month == "" && month <= 0 && month > 12){
+        alert("Enter valid month")
+        return false;
+    }else if (day == "" && date <= 0 && date > 31){
+        alert("Enter valid date")
+        return false;
+    }
+}
 
 function findDay() {
     day =  ( ( ( (cc/4) -(2*cc)-1) + ((5*yy/4) ) + ((26*(month+1)/10)) + date ) % 7 );
     return Math.floor(day);
-    if (day < 0 ){
-        day = day * -1;
-    }
-    else if (day > 0){
-        return day;
-    } 
 }
 
+var maleGender, femaleGender;
+
 function genderCheck(){
-    var gender = parseInt(prompt("Type 0 if you are male and 1 if you are female: "))
-    if (gender = 0 ){
-        var gen = "male"
-    }
-    else if (gender = 1 ){
-        var gen = "female"
-    }
-    else {
-        console.log("pass")
+    var maleGender = document.getElementById("male").checked;
+    var femaleGender = document.getElementById("female").checked;
+    if(maleGender == false && femaleGender == false){
+        alert("Please select an option");
+    }else if(maleGender == true){
+        document.write("You were born on a " + days[day] + "and your Akan name is " + mName[day] + ".");
+    }else if(femaleGender == true){
+        document.write("You were born on a " + days[day] + "and your Akan name is " + fName[day] + ".");
     }
 }
